0323efb628d7488a7d8f1ffa1a8585b6101214af
kim jong hwa <ehdxka3@naver.com>
2023-09-15 21:44:03 +0900

    first commit

---
+++ src/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import router from "./index.js";
+import dotenv from "dotenv";
+import errorhandler from "./middlewares/errorhandler.js";
+import log from "./middlewares/log.js";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(log);
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("hi");
+});
+app.use("/api", router);
+app.use((req, res, next) => {
+  res.status(404).send("Not Found");
+});
+app.use(errorhandler);
+
+app.listen(port, () => {
+  console.log(port, "번 서버가 열렸습니다");
+});
---
+++ src/middlewares/errorhandler.js
@@ -0,0 +1,15 @@
+import { CustomError } from "../errors/customError.js";
+
+export default function (err, req, res, next) {
+    console.error(`${req.method} ${req.originalUrl} : ${err}`);
+
+  if (err instanceof CustomError) {
+    return res.status(err.status).json({ errorMessage: err.message })
+  };
+
+  if (err.name === 'ValidationError') { // joi의 validationError
+    return res.status(412).json({ errorMessage: err.details[0].message })
+  };
+  
+  return res.status(500).json({ errorMessage: '서버 내부 에러가 발생했습니다.' });
+};
